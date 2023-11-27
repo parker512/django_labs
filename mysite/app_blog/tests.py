85b346b7565ef3fd118a549488f5f5360b53af4a
parker512 <alexdeineko830@gmail.com>
2023-11-28 00:34:35 +0200

    add test YourTestCase
    Signed-off-by: parker512 <alexdeineko830@gmail.com>

--- mysite/app_blog/tests.py
+++ mysite/app_blog/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class HomeTests(TestCase):
+    def test_home_view_status_code(self):
+        url = reverse('home')
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+
+class YourTestCase(TestCase):
+    def test_something(self):
+        self.assertTrue(True)
