cf63d618b3ceeeb1ea65d7329a2b160e7a996ef2
parker512 <alexdeineko830@gmail.com>
2023-11-28 00:23:09 +0200

    add можливість повернутись зі сторінки перегляду списку публікацій на головну.
    Signed-off-by: parker512 <alexdeineko830@gmail.com>

--- mysite/app_blog/urls.py
+++ mysite/app_blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import HomePageView, ArticleList, ArticleCategoryList, ArticleDetail
 
 urlpatterns = [
-    path(r'', HomePageView.as_view()),
+    path(r'', HomePageView.as_view(), name='home'),
     path(r'articles', ArticleList.as_view(), name='articles-list'),
     path(r'articles/category/<slug>', ArticleCategoryList.as_view(), name='articles-category-list'),
     path(r'articles/<year>/<month>/<day>/<slug>', ArticleDetail.as_view(), name='news-detail'),
