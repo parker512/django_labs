e32db4353a6f975bfd3199ed418012fc1ea80cbd
parker512 <alexdeineko830@gmail.com>
2023-11-27 21:01:52 +0200

    makemigrations amd migrate
    Signed-off-by: parker512 <alexdeineko830@gmail.com>

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -110,7 +110,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -118,7 +118,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_ROOT = BASE_DIR / 'static'
-STATICFILES_DIRS = [BASE_DIR / 'static', ]
+# STATICFILES_DIRS = [BASE_DIR / 'static', ]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
